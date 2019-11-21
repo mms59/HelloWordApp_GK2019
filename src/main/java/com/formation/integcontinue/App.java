d7a57f4dc01d88024c149b6dcfae993b12a14d8f
mms <toubatayssir@gmail.com>
2019-11-21 10:38:55 +0100

    initialisation du depot

---
+++ src/main/java/com/formation/integcontinue/App.java
@@ -0,0 +1,13 @@
+package com.formation.integcontinue;
+
+/**
+ * Hello world!
+ * modification5
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
